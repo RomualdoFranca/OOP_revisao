f37cc5c34906e8c777b550d6696542efb3b74f0b
RomualdoFranca <romualdo.ads2022@gmail.com>
2022-09-30 17:16:22 -0300

    criando construtores

--- src/constructorsPart1/Application.java
+++ src/constructorsPart1/Application.java
@@ -4,14 +4,29 @@ public class Application {
 
     public static void main(String[] args) {
 
-        BankAccount client1 = new BankAccount();
-        client1.setAccountNumber("001");
-        client1.setBalance(100.00);
-        System.out.println("Saldo inicial: " + client1.getBalance());
-        client1.setCustomerName("Romualdo França");
-        client1.setPhoneNumber("8199999999");
-        client1.setEmail("romualdo@email");
-        client1.deposit(50.00);
+        BankAccount client1 = new BankAccount("001", 0.00,
+                "Romualdo", "romualdo@email", "987654" ); // 'BankAccount()' executa o construtor automaticamente
+
+        // Fazendo o output das variaveis de instacias ja que não ha nenhum 'sout' dentro dos getters,
+        System.out.println(client1.getAccountNumber());
+        System.out.println(client1.getCustomerName());
+        // Nao precisa 'sout' aqui pois ja estao contidos nos metodos na classe BankAccount
+        client1.deposit(500.00);
+        client1.withdrawal(140.0);
+
+        // Atribuindo os valores nas variaveis de instancia sem construtor
+//        client1.setAccountNumber("001");
+//        client1.setBalance(100.00);
+//        System.out.println("Saldo inicial: " + client1.getBalance());// Usa-se o .get para 'pegar' o valor e fazer o output
+//        client1.setCustomerName("Romualdo França");
+//        client1.setPhoneNumber("8199999999");
+//        client1.setEmail("romualdo@email");
+
+        // Chamado o metodo de deposito. O valor atribuido aqui, vai ser guardado na variavel 'depositAmount'
+//        client1.deposit(50.00);
+//        client1.withdrawal(140.0);
+
+
 
 
     }
--- src/constructorsPart1/BankAccount.java
+++ src/constructorsPart1/BankAccount.java
@@ -7,9 +7,38 @@ public class BankAccount {
     private String customerName;
     private String email;
     private String phoneNumber;
+
+//  O construtor serve para iniciar o objeto que foi criado
+    public BankAccount() {
+        System.out.println("Empty constructor called");
+    }
+    public BankAccount(String accountNumber, double balance, String customerName,
+                       String email, String phoneNumber) {
+
+        System.out.println("Account constructor with parameters called");
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.customerName = customerName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
 //  Esse metodo nao retorna nenhum valor. Apenas atualiza o atributo 'balance'
+// O metodo 'void' atualiza as variaveis de instancia
     public void deposit(double depositAmount) {
          this.balance += depositAmount;
+        System.out.println("Deposit of " + depositAmount + " made. New balance is " + this.balance);
+    }
+    public void withdrawal(double withdrawlAmount) {
+//  A conta nao pode ser zerada. Se o valor do saque for maior ou igual ao saldo, o saque nao sera realizado
+        if (withdrawlAmount >= this.balance) {
+            System.out.println("Insuficient found");
+        }
+        else {
+            this.balance -= withdrawlAmount;
+            System.out.println("Saldo após saque: " + this.balance);
+        }
+
     }
 
     public String getAccountNumber() {
