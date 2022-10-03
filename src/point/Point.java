258630d9855e17a7abd41ddd89127bd4d46ca363
RomualdoFranca <romualdo.ads2022@gmail.com>
2022-10-03 19:01:53 -0300

    implemented the first method

--- src/point/Point.java
+++ src/point/Point.java
@@ -28,4 +28,24 @@ public class Point {
     public void setY(int y) {
         this.y = y;
     }
+    public double distance() {
+        /*'this Point' são as variaveis de instancias x e y.
+        * xA = this.x
+        * yA = this.y
+        * xB = 0.0
+        * yB = 0.0
+        * d(A,B)=√ (xB − xA) * (xB - xA) + (yB − yA) * (yB - yA)
+        * d(A,B) =√ (0.0 - this.x) * (0.0 - this.x) + (0.0 - this.y) * (0.0 - this.y)
+        *  */
+        // primeiro implementa o Math.pow. Depois o Math.sqrt
+        return Math.sqrt(Math.pow((0.0 - this.x), 2) + Math.pow((0.0 - this.y), 2));
+    }
+    public double distance(int x, int y) {
+
+        return -1;
+    }
+    public double distance(Point another) {
+
+        return -1;
+    }
 }
